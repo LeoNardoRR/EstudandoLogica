f5004c759bb4adc69dff20053bb41874b3afecae
Leonardo R <103374280+LeoNardoRR@users.noreply.github.com>
2022-11-23 22:25:15 -0300

    Add files via upload

--- JavaScript/Imposto/imposto.js
+++ JavaScript/Imposto/imposto.js
@@ -2,7 +2,6 @@
 //onde cada estado aplica uma alíquota variada. Construa um programa que receba o valor do produto e o estado de venda, calcule o valor 
 //do imposto e exiba o valor final do produto conforme a tabela abaixo:
 
-
 //MG -> 7%
 //SP -> 12%
 //RJ -> 15%
@@ -11,21 +10,49 @@
 //SC -> 18%
 
 function calcular() {
-    const valor = document.getElementById('valor')//valor digitado no input
-    const estado = document.getElementById('estado') //
+    const valor = document.getElementById('valor').value ;//valor digitado no input
+    const estado = document.getElementById('estado').value; //
     const resultado = document.getElementById('resultado');
-    const number = parseInt(valor.value)
-
-    if (number <= 0 || Number.isNaN(number)) {
-        console.error('Valor ' + valor.value + ' inválido')
+    
+    console.log('Valor: ' + valor + '| estado: ' + estado + '| resultado: ' + resultado +'| number: ' );
+    if (valor <= 0 || isNaN(valor)) {
+        console.log('Valor ' + valor + ' inválido');
         return;
     }
 
     resultado.innerHTML = '<i>Resultado:</i>';
 
-    if(estado == MG){
-        const calculo = valor * 0.7;
-        resultado.innerHTML = `<b>${valor}</b><br>`    
-        return resultado;
+    if(estado.toUpperCase() == 'MG'){
+        const calculo = parseInt(valor) + parseInt((valor * 0.07));
+        resultado.innerHTML = `<b>${calculo}</b><br>`;    
+        return calculo;
+    }
+    if(estado.toUpperCase() == 'SP'){
+        const calculo = parseInt(valor) + parseInt((valor * 0.12));
+        resultado.innerHTML = `<b>${calculo}</b><br>`;    
+        return calculo;
+    }
+    if(estado.toUpperCase() == 'RJ'){
+        const calculo = parseInt(valor) + parseInt((valor * 0.15));
+        resultado.innerHTML = `<b>${calculo}</b><br>`;    
+        return calculo;
+    }
+    if(estado.toUpperCase() == 'MS'){
+        const calculo = parseInt(valor) + parseInt((valor * 0.08));
+        resultado.innerHTML = `<b>${calculo}</b><br>`;    
+        return calculo;
+    }
+    if(estado.toUpperCase() == 'ES'){
+        const calculo = parseInt(valor) + parseInt((valor * 0.12));
+        resultado.innerHTML = `<b>${calculo}</b><br>`;    
+        return calculo;
+    }
+    if(estado.toUpperCase() == 'SC'){
+        const calculo = parseInt(valor) + parseInt((valor * 0.18));
+        resultado.innerHTML = `<b>${calculo}</b><br>`;    
+        return calculo;
+    }
+    else
+    {
     }
 }
