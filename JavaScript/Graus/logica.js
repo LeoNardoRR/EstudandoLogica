43cfe8074d8a7af5f9d9759784508603d869756f
Leonardo R <103374280+LeoNardoRR@users.noreply.github.com>
2022-12-03 19:53:46 -0300

    Add files via upload

--- JavaScript/Graus/logica.js
+++ JavaScript/Graus/logica.js
@@ -1,26 +1,13 @@
-//Faça um programa que peça a temperatura em graus Fahrenheit (°F),
-//transforme e mostre a temperatura em graus Celsius (°C).
+let celsius = document.getElementById('celsius');
+let fahrenheit = document.getElementById('fahrenheit');
+let kelvin = document.getElementById('kelvin');
 
+celsius.oninput = () => {
+    let output = (parseFloat(celsius.value) * 9) / 5 + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+};
 
-    function temperatura(){
-        const temperatura = document.getElementById('temperatura');
-        const fahrenheit = document.getElementById("fahrenheit").value;
-        const celsius = document.getElementById("celsius").value;
-        const resultado = document.getElementById('resultado').value;
-    
-        resultado.innerHTML = '<i>Resultado:</i>';
-
-    if(temperatura == 'F'){
-        const calculo = parseInt(valor) + parseInt((valor * 0.07));
-        resultado.innerHTML = `<b>${calculo}</b><br>`;    
-        return calculo;
-    }
-    else if(temperatura =='C'){
-        celsius = (parseInt(fahrenheit) - 32) * 1.8;
-    }
-    
-    else(isNaN(fahrenheit) || isNaN(celsius));{
-        alert("Digite um valor váilido!");
-        return 
-    }
+fahrenheit.oninput = () => {
+    let output = ((parseFloat(fahrenheit.value) - 32) * 5)/9;
+    fahrenheit.value = parseFloat(output.toFixed(2));    
 }
